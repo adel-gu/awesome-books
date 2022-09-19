41f2379b496d7688489bf1695eef7b04882fbd71
adelguit <adeelguitoun@gmail.com>
2022-09-19 13:30:56 +0100

    Apply book adding functionality

---
+++ scripts/main.js
@@ -0,0 +1,60 @@
+const bookForm = document.forms[0];
+const collections = document.querySelector('.collections');
+
+// add books to the window.
+// add book to local storage.
+// when the page load books from local storage then display books.
+
+// Get the user inputs
+function getNewBookData() {
+  const booktTitle = bookForm.title.value;
+  const bookAuthor = bookForm.author.value;
+
+  const book = {
+    title: booktTitle,
+    author: bookAuthor,
+  };
+
+  return book;
+}
+
+// Add the new book to local storage
+function addNewBook(e) {
+  e.preventDefault();
+  saveBook(getNewBookData());
+  bookForm.submit();
+}
+
+// save the book in local sotrage
+function saveBook(book) {
+  let books = [];
+  if (localStorage.getItem('Books')) {
+    books = JSON.parse(localStorage.getItem('Books'));
+  }
+
+  books.push(book);
+  localStorage.setItem('Books', JSON.stringify(books));
+}
+
+// Display the collections of books
+function displayBook() {
+  if (localStorage.getItem('Books')) {
+    const books = JSON.parse(localStorage.getItem('Books'));
+    books.forEach((book) => {
+      const bookTemplate = `
+        <tr class="book-info">
+          <td class="title">${book.title}</td>
+          <td class="author">${book.author}</td>
+          <td><button class="remove-book">remove</button></td>
+        </tr>
+    `;
+
+      collections.innerHTML += bookTemplate;
+    });
+  }
+}
+
+displayBook();
+bookForm.addEventListener('submit', addNewBook);
+
+// Remove Book
