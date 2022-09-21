cb4f09d8c452e94733cf63a7e12800fd63dce66a
adelguit <adeelguitoun@gmail.com>
2022-09-21 14:21:48 +0100

    Create BookApp class for adding and removing books from screen, and apply the add functionality

--- scripts/main.js
+++ scripts/main.js
@@ -9,3 +9,37 @@ class Book {
     return { title: this.title, author: this.author };
   }
 }
+
+class BookApp {
+  // get book template
+  static #bookTemplate(book) {
+    const bookTemplate = `
+       <tr class="book-info">
+         <td class="title">${book.title}</td>
+         <td class="author">${book.author}</td>
+         <td><button class="remove">Remove</button></td>
+       </tr>
+   `;
+    return bookTemplate;
+  }
+
+  // Add Book to the UI
+  static displayBook(container, book) {
+    container.innerHTML += BookApp.#bookTemplate(book);
+  }
+}
+
+const bookForm = document.forms[0];
+const collections = document.querySelector('.collections');
+
+// Display books when page is reloading
+bookForm.add.addEventListener('click', (e) => {
+  e.preventDefault();
+  const title = bookForm.title.value;
+  const author = bookForm.author.value;
+
+  const book = new Book(title, author);
+  BookApp.displayBook(collections, book);
+
+  bookForm.reset();
+});
