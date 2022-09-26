685444f0fe89da7b033ded9dbfbd3bdbfe82273d
adelguit <adeelguitoun@gmail.com>
2022-09-26 12:50:27 +0100

    Add time and date function

--- scripts/index.js
+++ scripts/index.js
@@ -5,9 +5,15 @@ import * as Base from './modules/base.js';
 const bookForm = document.forms[0];
 const collections = document.querySelector('.collections');
 const navLinks = document.querySelectorAll('.nav-link');
+const date = document.querySelector('.date');
 
 // App functions.
 Base.addBook(bookForm, collections);
 Base.removeBook(collections);
 Base.loadBooks(collections);
 Base.spa(navLinks);
+
+// Set date and time
+setInterval(() => {
+  Base.dateTime(date);
+}, 0);
